baad5ea9bb48a9150e0c66050ee62c5e88e64ef9
Josh Youngman <thejoshy01@live.co.uk>
2018-01-18 15:45:19 +0000

    Firebase Integration

---
+++ public/JS/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyB78hKd1f05HY4iqZ1t2hyi3kG_BbqLPgM",
+    authDomain: "georges-chauffeurs.firebaseapp.com",
+    databaseURL: "https://georges-chauffeurs.firebaseio.com",
+    projectId: "georges-chauffeurs",
+    storageBucket: "",
+    messagingSenderId: "171113033722"
+  };
+  firebase.initializeApp(config);
