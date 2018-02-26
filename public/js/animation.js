9a087dd740c1816ceaf140e58fc6b8e8c05af54c
Josh Youngman <thejoshy01@live.co.uk>
2018-02-26 17:27:50 +0000

    Firebase update test

--- public/js/animation.js
+++ public/js/animation.js
@@ -1,4 +1,4 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function($) {
     $(function animation() {
         var emailPasswordLeave = 'animate bounceOutLeft';
         var userDetailsJoin = 'animate bounceInRight';
